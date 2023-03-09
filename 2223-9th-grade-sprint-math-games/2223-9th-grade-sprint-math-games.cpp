67caf13961327de3efa3d7729e3b74ad289d81b9
IVBadalov21 <IVBadalov21@codingburgas.bg>
2023-03-09 11:03:57 +0200

    Set up raylib

--- 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
+++ 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
@@ -1,20 +1,28 @@
-// 2223-9th-grade-sprint-math-games.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include "raylib.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+
+	InitWindow(screenWidth, screenHeight, "Open-minded project game");
+
+	SetTargetFPS(60);
+	
+	while (!WindowShouldClose())
+	{
+
+		BeginDrawing();
+
+		ClearBackground(LIGHTGRAY);
+
+		DrawText("First steps in raylib", 190, 200, 20, ORANGE);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
