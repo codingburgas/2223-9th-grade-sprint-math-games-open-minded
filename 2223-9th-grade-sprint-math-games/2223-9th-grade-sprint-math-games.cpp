f24f78950349c923dfc736c572234191fc90d2fc
TDTanev21 <TDTanev21@codingburgas.bg>
2023-03-19 18:11:33 +0200

    The game's Main Menu

--- 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
+++ 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
@@ -1,38 +1,37 @@
 #include "raylib.h"
+#include <iostream>
 
-int main() {
-
-	InitWindow(GetScreenWidth(), GetScreenHeight(), "Open-minded project game");
-
-	SetTargetFPS(60);
-	
-	Texture2D learn, test, about, minimize, fullscreen, close;
-	learn = LoadTexture("../images/learn.png");
-	test = LoadTexture("../images/test.png");
-	about = LoadTexture("../images/about.png");
-	minimize = LoadTexture("../images/minimize.png");
-	fullscreen = LoadTexture("../images/fullscreen.png");
-	close = LoadTexture("../images/close.png");
-
+void mainMenu()
+{
+	Texture2D mainMenu, startGame, rules, quitGame;
+	mainMenu = LoadTexture("../images/mainMenu.png");
+	startGame = LoadTexture("../images/start.png");
+	rules = LoadTexture("../images/rules.png");
+	quitGame = LoadTexture("../images/quitGame.png");
 	while (!WindowShouldClose())
 	{
-		
 		BeginDrawing();
 
-		ClearBackground(BACKGOUND_COLOR);
+		ClearBackground(BLACK);
 
-		DrawTexture(learn, 50, 100, WHITE);
-		DrawTexture(test, 550, 100, WHITE);
-		DrawTexture(about, 1050, 100, WHITE);
-		DrawTexture(minimize, 1630, 40, WHITE);
-		DrawTexture(fullscreen, 1730, 40, WHITE);
-		DrawTexture(close, 1830, 40, WHITE);
+		DrawTexture(mainMenu, 50, 100, WHITE);
+		DrawTexture(startGame, 770, 350, WHITE);
+		DrawTexture(rules, 770, 475, WHITE);
+		DrawTexture(quitGame, 775, 600, WHITE);
+
 
 		EndDrawing();
 	}
+}
 
-	CloseWindow();
+int main()
+{
 
-	return 0;
+	InitWindow(GetScreenWidth(), GetScreenHeight(), "Open Minded - game project");
+
+	SetTargetFPS(60);
+
+	mainMenu();
+
 }
 
