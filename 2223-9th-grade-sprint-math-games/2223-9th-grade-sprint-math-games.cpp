f8bb294bc949e8d9dafbaef7c9ac7c75619a0c1a
IVBadalov21 <IVBadalov21@codingburgas.bg>
2023-03-21 17:49:59 +0200

    Make the exit button work & Set exit keys

--- 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
+++ 2223-9th-grade-sprint-math-games/2223-9th-grade-sprint-math-games.cpp
@@ -5,6 +5,9 @@ using namespace std;
 void game()
 {
 	Texture2D tryout = LoadTexture("../images/Try.png");
+
+	SetExitKey(KEY_ESCAPE);
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
@@ -12,23 +15,23 @@ void game()
 		ClearBackground(BLACK);
 		DrawTexture(tryout, 50, 100, WHITE);
 		EndDrawing();
-
 	}
 }
+
 void rulesFunction()
 {
-	
+
 }
-void quitGameFunction()
+
+int main()
 {
-	
-}
-void mainMenu()
-{
+	InitWindow(GetScreenWidth(), GetScreenHeight(), "Open Minded - game project");
+
+	SetTargetFPS(60);
+
 	Texture2D background, startGame, rules, quitGame;
 	Vector2 mousePoint;
-	
-	 
+	bool exitGame = false;
 	background = LoadTexture("../images/mainMenuBackground.png");
 	startGame = LoadTexture("../images/start.png");
 	rules = LoadTexture("../images/rules.png");
@@ -36,7 +39,8 @@ void mainMenu()
 	Rectangle startGameButton = { 770, 350, startGame.width, startGame.height - 80 };
 	Rectangle rulesButton = { 770, 475, rules.width, rules.height - 80 };
 	Rectangle quitGameButton = { 775, 600, quitGame.width, quitGame.height - 80 };
-	while (!WindowShouldClose())
+
+	while (!exitGame)
 	{
 		mousePoint = GetMousePosition();
 
@@ -48,34 +52,18 @@ void mainMenu()
 		DrawTexture(startGame, 770, 350, WHITE);
 		DrawTexture(rules, 770, 475, WHITE);
 		DrawTexture(quitGame, 775, 600, WHITE);
-
-		
-
 		EndDrawing();
+
+		SetExitKey(0);
+
 		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mousePoint, startGameButton))
-		{
 			game();
-		}
+
 		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mousePoint, rulesButton))
-		{
 			rulesFunction();
-		}
+
 		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mousePoint, quitGameButton))
-		{
-			quitGameFunction();
-		}
-		
+			exitGame = true;
 	}
-}
-
-int main()
-{
-
-	InitWindow(GetScreenWidth(), GetScreenHeight(), "Open Minded - game project");
-
-	SetTargetFPS(60);
-
-	mainMenu();
-
 }
 
