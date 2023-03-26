51785805ce64355b5c73cc569b5f5f7980a2c92f
GNIvanov21 <gnivanov21@codingburgas.bg>
2023-03-27 02:39:04 +0300

    add random numbers algorithm

--- 2223-9th-grade-sprint-math-games/game.cpp
+++ 2223-9th-grade-sprint-math-games/game.cpp
@@ -1,17 +1,39 @@
 #include "raylib.h"
 #include "game.h"
 #include <iostream>
+#include <random>
+#include <string>
+#include <chrono>
 using namespace std;
 
+int numbers[50];
+
+void decToBin(int n)
+{
+	int numbers[100];
+	int i = 0;
+	while (n > 0) {
+
+		numbers[i] = n % 2;
+		n = n / 2;
+		i++;
+	}
+	for (int j = i - 1; j >= 0; j--)
+		cout << numbers[j];
+}
 void game()
 {
 	SetExitKey(KEY_ESCAPE);
 
-	Texture2D ship, meteor,background;
-	meteor = LoadTexture("../images/asteroid_model.png");
+	Texture2D ship;
 	ship = LoadTexture("../images/ship1.png");
-	background = LoadTexture("../images/gameBackground.png");
 	Vector2 ballPosition = { -100.0f, -100.0f };
+	int num, operation;
+	random_device(rd);
+	uniform_int_distribution <int> distNumber(0, 15);
+	uniform_int_distribution <int> distOperation(0, 5);
+
+
 	while (!WindowShouldClose())
 	{
 		ballPosition = GetMousePosition();
@@ -19,15 +41,17 @@ void game()
 		BeginDrawing();
 		DrawFPS(50, 50);
 
-		DrawTexture(background, 600, 300, WHITE);
-		DrawTexture(meteor, 600, 300, WHITE);
 		DrawTexture(ship, GetScreenWidth() / 3 + 90, 700, WHITE);
-		DrawText("Tedo e qk", 15, 320, 15, RED);
 		DrawCircleV(ballPosition, 10, MAROON);
+		operation = distOperation(rd);
+		num = distNumber(rd);
+		for (int i = 0; i < 4; i++)
+		{
+			decToBin(num);
+			string task = to_string(numbers[i]);
+		}
 		HideCursor();
 		ClearBackground(BLACK);
 		EndDrawing();
-
-
 	}
 }
